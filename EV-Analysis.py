cb13cd790561ef02823e9b21f84cd670c14a0bd5
David <david.donpauli@gmail.com>
2023-08-16 14:26:35 +0000

    reordered and cleaned script with chatgpt help

--- EV-Analysis.py
+++ EV-Analysis.py
@@ -10,8 +10,7 @@ from scipy import stats
 dataset = pd.read_csv("1_h_01.txt", sep="\t", header=1)
 
 # Convert string to floating point number
-dataset['mm'] = dataset["mm"].str.replace(',', '.')
-dataset['mm'] = dataset["mm"].astype(float)
+dataset['mm'] = dataset['mm'].str.replace(',', '.').astype(float)
 print(dataset)
 
 # Convert string to datetime object
@@ -27,14 +26,19 @@ formatter = mplt.dates.ConciseDateFormatter(locator)
 plt.show()
 
 # Plot a histogramm
+plt.hist(dataset['mm'], bins=20, edgecolor='black')
+plt.xlabel('Cumulated rainfall data [mm]')
+plt.ylabel('Frequency')
+plt.title('Histogram of Cumulated Rainfall Data')
+plt.show()
 
 # Calculate the ECDF
 res = stats.ecdf(dataset['mm'])
 print(res)
 
 # Plot the ECDF
-ax = plt.subplot()
-res.cdf.plot(ax)
+fig, ax = plt.subplots()
+ax.plot(res.x, res.y)
 ax.set_xlabel('Cumulated rainfall data [mm]')
 ax.set_ylabel('Empirical CDF')
 plt.show()
@@ -46,8 +50,7 @@ sample_var = dataset['mm'].var()
 print(f'the sample mean is {sample_mean:.2f}')
 print(f'the sample variance is {sample_var:.2f}')
 
-# Apply the Methods of Moments to calculate the location and scale parameter
-# of the Gumbel distribution
+# Apply the Methods of Moments to calculate Gumbel distribution parameters
 eulergamma = 0.57721566490
 scale_gumble = math.sqrt(6 * sample_var) / math.pi
 loc_gumbel = sample_mean - eulergamma * scale_gumble
@@ -78,7 +81,9 @@ ax.legend()
 plt.show()
 
 # Plot the Gumbel distribution COMPARE WITH HISTOGRAMM
-# ax = plt.subplot()
-# ax.set_xlabel('Gumbel probability density function')
-# ax.scatter(dataset['mm'], gumbel_r, label='gumbel_r pdf')
-# plt.show()
+fig, ax = plt.subplots()
+ax.plot(res.x, res.y, label='ECDF')              
+ax.plot(x, gumbel_r_cdf, label='Gumbel CDF', color='red')
+ax.legend()
+ax.set_xlabel('Cumulative density function')
+plt.show()
